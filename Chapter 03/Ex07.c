e1e62bdc8767eb5abc3a909b2956f75621463889
waltz26 <qq1932048447@163.com>
2021-04-17 00:02:25 +0800

    第三章

--- Chapter 03/Ex07.c
+++ Chapter 03/Ex07.c
@@ -5,7 +5,7 @@ int main(void)
 
     printf("Enter your height(inch): ");
     scanf("%f",&inch);
-    printf("Your height is %fcm.",2.54*inch);
+    printf("Your height is %.1fcm.",2.54*inch);
     
     return 0;
 }
