a63cee09167960a60a2cc83bf6539a182eb06faf
waltz26 <qq1932048447@163.com>
2021-05-12 11:44:18 +0800

    第三章

--- Chapter 03/Ex01.c
+++ Chapter 03/Ex01.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
 int main(void)
 {
+    //一些值
+    printf("Int type integer maximum value:%d\n",__INT_MAX__);    //int类型整数最大值
+    printf("Float type floating-point number maximum value:%e\n",__FLT_MAX__);    //float类型浮点数最大值
+    printf("Float type floating-point number minimun value:%e\n",__FLT_MIN__);    //float类型浮点数最小值
+    printf("\n");
+
     //用了书上的示例
-    int i_value=3000000000;    //2147483647为32位带符号整型‬的上限值‬
-    float toobig=3.4e37*100.0f;    //单精度浮点型的上限值至少为10的37次方级(基本在3.4E38这个值)
-    float toosmall=1.23456e-37/10;    //单精度浮点型的下限值至多为10的-37次方级，有效数字6位
+    int i_value=3000000000;    //整数上溢
+    float toobig=3.4e37*100.0f;    //浮点数上溢
+    float toosmall=1.175494e-38/10;    //浮点数下溢
 
     printf("i_value=%d\n",i_value);
-    printf("toobig=%e toosmall=%e\n",toobig,toosmall);
+    printf("toobig=%e, toosmall=%e\n",toobig,toosmall);
 
     //GCC编译运行结果：
     //i_value=-1294967296
-    //toobig=1.#INF00e+000 toosmall=1.234560e-038
+    //toobig=1.#INF00e+000, toosmall=1.175495e-039(不知道为什么会显示这样，正常结果应该是0.117549e-038)
     //clang未测试
     return 0;
 }
--- Chapter 03/Ex03.c
+++ Chapter 03/Ex03.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-    printf("\aStartled by the sudden sound, Sally shouted,\n");
+    printf("\aStartled by the sudden sound, Sally shouted,\n");    //计算机对于警报字符没有任何表示
     printf("\"By the Great Pumpkin, what was that!\"\n");
     
     return 0;
